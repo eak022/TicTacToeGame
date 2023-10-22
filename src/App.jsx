b2b7e73a13008e031c9d66d03ae99227c469ff28
eak022 <eakeakkachai71180@gmail.com>
2023-10-22 18:52:00 +0700

    f

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TicTacToe from "./components/TicTacToe";
+import "./App.css";
+import circle_icon from "/assets/circle.png";
+import cross_icon from "/assets/cross.png";
+
+function App() {
+  return (
+    <>
+      <TicTacToe />
+    </>
+  );
+}
+
+export default App;
